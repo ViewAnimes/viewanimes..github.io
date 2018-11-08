d0e7609517b418d4028ac0da112717645dab0903
ViewAnimes <34377966+ViewAnimes@users.noreply.github.com>
2018-11-08 01:50:58 -0300

    Create sistema3.js

---
+++ sistema3.js
@@ -0,0 +1,20 @@
+$(function() {var videohtml5 = $('.videoGallery .videohtml5');
+var liHeight = $('.videoGallery li').height();
+
+// BLOGGER
+videohtml5.click(function(){
+var videoID = $(this).attr('data-videoID');
+var videos = $('<div class="meuVideo"><center>MEU NOME</center> </div>');
+
+$('.meuVideo, .nowPlaying').remove();
+$(this).parents().eq(2).append(videos);
+
+});
+
+// Fechar Videos
+$('.close').click(function(){
+$('.meuVideo, .nowPlaying').remove();
+});
+ 
+
+})
