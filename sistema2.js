0e20efa2da20e61dd93fd8fe74e828db2501ea09
ViewAnimes <34377966+ViewAnimes@users.noreply.github.com>
2018-11-08 01:44:29 -0300

    Create sistema2.js

---
+++ sistema2.js
@@ -0,0 +1,20 @@
+$(function() {var videohtml5 = $('.videoGallery .videohtml5');
+var liHeight = $('.videoGallery li').height();
+
+// BLOGGER
+videohtml5.click(function(){
+var videoID = $(this).attr('data-videoID');
+var videos = $('<div class="meuVideo"><center><object id="BLOG_video-db1f11d8c00a7500" class="BLOG_video_class" contentid="db1f11d8c00a7500" width="320" height="266" ></object></center> </div>');
+
+$('.meuVideo, .nowPlaying').remove();
+$(this).parents().eq(2).append(videos);
+
+});
+
+// Fechar Videos
+$('.close').click(function(){
+$('.meuVideo, .nowPlaying').remove();
+});
+ 
+
+})
